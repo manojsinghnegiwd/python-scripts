d172a099c200350ef0c689b6580467c9bdb79e95
Manoj Singh Negi <justanothermanoj@gmail.com>
2018-02-17 15:15:53 +0530

    filtered empty links

--- webCrawler.py
+++ webCrawler.py
@@ -4,11 +4,15 @@ from lxml import html, etree
 
 website = sys.argv[1]
 
-#storing response
+# storing response
 response = requests.get(website)
 tree = html.fromstring(response.text)
 
+# stroing all links
 alllinks = tree.xpath('.//a/@href')
+
+# filter all empty links
+alllinks = [link for link in alllinks if not link == '#']
 
 for link in alllinks:
 	print(link)
